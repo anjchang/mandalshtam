ea46345026a81c0602830eea1a0861c9a43eeecd
Angela Chang <anjchang@gmail.com>
2016-11-28 04:30:30 -0500

    textblob run earlier
    run Textblob earlier so classification is in context of the source text
    use keywords to filter out the wordlist

--- makewordlist.py
+++ makewordlist.py
@@ -1,40 +1,46 @@
+#python 2.7 
 import glob
 import re
 import nltk
+from textblob import TextBlob 
 
 from nltk.corpus import stopwords
 
-#look in source directory
 glob.glob("source/*.txt")
 file_list = glob.glob("source/*.txt")
+print "processing "
 print file_list
+print '\r\n'
 
-def clean(text):
-    words = re.findall('[A-Za-z]+',text)  #remove punctuations
+def classify(source):  #create TextBlob object from source
+    blob = TextBlob(source)
+    return blob
+
+def cleanblob(sourceblob): #clean the text and return wordlist
+    print sourceblob
+    print '\r\n'
+    sourcewords =  str(sourceblob.words)
+    words = re.findall('[A-Za-z]+',sourcewords)  #remove punctuations
     words = list(set(words))  #remove duplicate 
     # remove all stopwords and uppercase words that are stopwords
     words = [w for w in words if not w.lower() in stopwords.words('english')]
-    return words
-    
-#get text from files
+    words.sort()
+    return words 
+
+
 useful_words = []
 for filename in file_list:
     source = open(filename)
     text = source.read()
-    useful_words += clean(text)
-
-#sort the words in alphabetica order
-useful_words.sort()
-print str(len(useful_words)) + " useful words"
-
-#use textblob classifier
-from textblob import TextBlob 
-usefulblob =  TextBlob(" ".join(useful_words)) 
-
-#create output file
-output_file = open('classified.txt','w')
-for (word,tag) in usefulblob.tags:
-    print word, tag
-    output_file.write('%s %s \r\n' %( word, tag))
+    text = text.decode('ascii', errors="ignore") #get rid of unicode errors for extblob    
+    sourceblob = classify(text) #classify poems
+    words = cleanblob(sourceblob) #get keywords 
+    output_file = open('output.txt','w')
+    for keyword in words:
+        for (word,tag) in sourceblob.tags:
+            if keyword == word:
+                print word,tag
+                output_file.write('%s %s\r\n' %( word, tag))
+    output_file.close
     
-output_file.close()
+print "done, look in output.txt"
